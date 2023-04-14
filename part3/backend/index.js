1c8ece8c66af418966116dca0f5878d9b1878f64
ybted <ybted@github.com>
2023-04-14 16:44:45 +0800

    uibuild

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -39,22 +39,19 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-  if (!body.content) {
-      return response.status(400).json({
-          error: 'content missing'
-      })
-  }    
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body 
 
   const note = new Note({
       content: body.content,
       important: body.important || false,
   })
   
-  note.save().then(savedNote => {
+  note.save()
+    .then(savedNote => {
     response.json(savedNote)
   })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -66,14 +63,13 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-  console.log(body)
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
+  const { content, important } = request.body
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: 'query' }
+    )
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -93,7 +89,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name == 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
